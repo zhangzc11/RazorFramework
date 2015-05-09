8a42e4f802ff4c38ebffc0a5420ecb2344aec519
Cristian Pena <cmorgoth@dhcp-112-223.caltech.edu>
2015-05-09 16:55:08 -0700

    Adding Aux, Implementing ListSelectedSusyHggTree

--- Tools/SyncTools/app/CompareEvents.cc
+++ Tools/SyncTools/app/CompareEvents.cc
@@ -7,6 +7,8 @@
 #include "ntp1.hh"
 #include "aux.hh"
 
+#define _debug 1
+
 int main( int argc, char* argv[] )
 {
 
@@ -24,12 +26,44 @@ int main( int argc, char* argv[] )
       return 0;
     }
   
-  std::string fname1 = "";
-  std::string fname2 = "";
+  std::string fname1 = ParseCommandLine( argc, argv, "-file1=" );
+  std::string fname2 = ParseCommandLine( argc, argv, "-file2=" );
 
-  fname1 = ParseCommandLine( argc, argv, "-file1=" );
-  fname2 = ParseCommandLine( argc, argv, "-file2=" );
-
+  if ( fname1 == "" || fname2 == "" )
+    {
+      std::cerr <<  "[ERROR]: please provide two files with run and events" << std::endl;
+      return -1;
+    }
+  
+  // D e f i n e   o u t p u t   f i l e   n a m e s
+  //-----------------------------------------------
+  std::string outname1;
+  std::string outname2;
+  //First output file
+  if ( fname1.find( "." ) != std::string::npos )
+    {
+      outname1 = fname1.substr( 0, fname1.find_last_of( '.' ) ) + "_Missing.txt";
+      if ( _debug ) std::cout << "[DEBUG]: output file name: " << outname1 << std::endl;
+    }
+  else
+    {
+      outname1 = fname1 + "_Missing.txt";
+      if ( _debug ) std::cout << "[DEBUG]: output file name: " << outname1 << std::endl;
+    }
+  
+  //First output file                                                                             
+  if ( fname2.find( "." ) != std::string::npos )
+    {
+      outname2 = fname2.substr( 0, fname2.find_last_of( '.' ) ) + "_Missing.txt";
+      if ( _debug ) std::cout << "[DEBUG]: output file name: " << outname2 << std::endl;
+    }
+  else
+    {
+      outname2 = fname2 + "_Missing.txt";
+      if ( _debug ) std::cout << "[DEBUG]: output file name: " << outname2 << std::endl;
+    }
+  
+  
   std::map < std::string, evt > map1 = MakeMap( fname1 );
   std::map < std::string, evt > map2 = MakeMap( fname2 );
   CreateDiff( map1, map2, "HggRazorMissing.txt" );
