8f8ad1a51b5b898fc6e7e403655baf63a0113746
Cristian <cristian.pena@caltech.edu>
2015-05-11 11:08:47 -0700

    adding changes to submit t3-higgs

--- Tools/SkimEvents/SubmitT3-higgsCondor.sh
+++ Tools/SkimEvents/SubmitT3-higgsCondor.sh
@@ -5,12 +5,13 @@ import sys
 #######################################
 ### usage  cmst3_submit_manyfilesperjob.py dataset njobs applicationName queue 
 #######################################
-if len(sys.argv) < 5:
-    print "usage SubmitT3-higgsCondor.sh ProcessName sampleList.list files_per_job hadoopDirName tarball pick_events.txt"
-    print "e.g: python SubmitT3-higgsCondor.sh ProcessName list.txt  files_per_job hadoopDirName tarball  pick_events.txt"
+if len(sys.argv) < 10:
+    print "usage SubmitT3-higgsCondor.sh ProcessName sampleList.list files_per_job hadoopDirName tarball pick_events.txt treeName runBr\
+anch eventBranch"
+    print "e.g: python SubmitT3-higgsCondor.sh ProcessName list.txt  files_per_job hadoopDirName tarball  pick_events.txt treeName runBranch eventBranch"
     sys.exit(1)
 
-tarball = "vecbosappDoubleMu.tar" 
+tarball = "" 
 
 os.system("export PATH=$PATH:/opt/gridengine/bin/lx26-amd64/")
 
@@ -28,6 +29,10 @@ files_per_job    = int(sys.argv[3])
 hadoopDir        = sys.argv[4]
 tarball          = sys.argv[5]
 pick_events      = sys.argv[6]
+treeName         = sys.argv[7]
+runBranch        = sys.argv[8]
+eventBranch      = sys.argv[9]
+
 if pick_events.find( "/home/cmorgoth/" ) == -1:
     pick_events = pwd + "/" + pick_events
 #######################################
@@ -78,6 +83,7 @@ for ijob in range(njobs):
     outputName = "submission/"+process+"/src/submit_"+str(ijob)+".sh"
     logName = "submission/"+process+"/log/submit_"+str(ijob)
     submitName = "submission/"+process+"/src/condor_"+str(ijob)+".sub"
+    rootOutput = "skim_" + treeName + "_" + str(ijob)+ ".root"
     #print "OUTPUTFILE: ", outputName
     submitFile = open(submitName,'w')
     submitFile.write("Executable = "+pwd+"/"+outputName)
@@ -95,17 +101,22 @@ for ijob in range(njobs):
     outputfile.write("mkdir "+process+"_"+str(ijob))
     outputfile.write("\ncd "+process+"_"+str(ijob))
     outputfile.write("\ncmsrel CMSSW_6_2_0;cd CMSSW_6_2_0/src;cmsenv\n")
-    outputfile.write("cp /home/cmorgoth/"+tarball+" .;tar -xvf "+tarball+"; cd SkimEvents\n")
+    outputfile.write("tar -xf "+tarball+"; cd SkimEvents\n")
     outputfile.write("make clean;make -j 4\n")
-    outputfile.write("mkdir out;cp "+inputfilename+" .\n")
-    outputfile.write("cp " + pick_events + " pick_events_"+ str(ijob) + ".list\n")
-    outputfile.write("./CreateSkimmed pick_events_"+str(ijob)+".list" + " input_"+str(ijob)+".list pick_events_small_" + process + "_" + str(ijob) + ".root\n")
+    outputfile.write("cp " + inputfilename + " input_list.txt\n")
+    outputfile.write("cp " + pick_events + " pick_events.txt\n")
+    outputfile.write( "\n./CreateSkimmed --event_list==pick_events.txt"
+                      + " --list_of_ntuples==input_list.txt"
+                      + " --output_name==" + rootOutput
+                      + " --tree_name==" + treeName
+                      + " --run_branch==" + runBranch
+                      + " --event_branch==" + eventBranch
+                      )
     outputfile.write("\nmkdir -p /mnt/hadoop/store/user/cmorgoth/"+hadoopDir)
     outputfile.write("\nmkdir -p /mnt/hadoop/store/user/cmorgoth/"+hadoopDir+"/"+process)
-    outputfile.write("\ncp pick_events_small_" + process + "_" + str(ijob) + ".root /mnt/hadoop/store/user/cmorgoth/"+hadoopDir+"/"+process+"/.")
-    #outputfile.write("\ncp out/"+process+"_"+str(ijob)+".root /mnt/hadoop/store/user/cmorgoth/"+hadoopDir+"/"+process+"/.")   
+    outputfile.write("\ncp " + rootOutput + "/mnt/hadoop/store/user/cmorgoth/"+hadoopDir+"/"+process+"/.")
     
-    outputfile.close
+    outputfile.close()
     os.system("sleep .1\n")
     scriptfile=pwd+"/"+outputName
     submitjobs.write("condor_submit "+pwd+"/"+submitName)
@@ -113,4 +124,5 @@ for ijob in range(njobs):
     ijob = ijob+1
     continue
 
+submitjobs.close()
 
