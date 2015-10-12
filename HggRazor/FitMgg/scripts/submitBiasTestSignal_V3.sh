d30f941db6bc2e834180389d187adc7bb6c5129c
Cristian <cristian.pena@caltech.edu>
2015-10-12 14:57:06 -0700

    updating all

--- HggRazor/FitMgg/scripts/submitBiasTestSignal_V3.sh
+++ HggRazor/FitMgg/scripts/submitBiasTestSignal_V3.sh
@@ -4,11 +4,12 @@ echo "RUNNING BIAS TEST"
 motherDir=/home/cmorgoth/git/RazorFramework/
 workingDir=/home/cmorgoth/git/RazorFramework/HggRazor/FitMgg/
 category="highres"
-fitName=("singleExp" "doubleExp" "modExp" "singlePow" "doublePow" "poly2" "poly3")
+fitName=("singleExp" "doubleExp" "modExp" "singlePow" "doublePow" "poly2" "poly3" "poly4")
 MR=350.0
 RSQ=0.035
 Iso="no"
-outDir="BiasSignal_NoInvIso_GB_SBF"
+sfrac=1.0
+outDir="BiasSignal_NoInvIso_GB_SBF_SoverB_1p0"
 ctr=0
 cd $workingDir
 mkdir -p submission
@@ -22,7 +23,7 @@ for i in ${fitName[@]};do
 	subOut="submission/biasTest_"$i"_"$j".out"
 	subError="submission/biasTest_"$i"_"$j".err"
 	tmpDir="biasTestSignal_"$i"_"$j
-	echo -e "#!/bin/sh\nexport SCRAM_ARCH=slc5_amd64_gcc472\nsource /cvmfs/cms.cern.ch/cmsset_default.sh\nexport LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/cmorgoth/git/BSMatLHC/hepmc/lib/\nmkdir $tmpDir\ncd $tmpDir\n\ncmsrel CMSSW_6_2_0;cd CMSSW_6_2_0/src;cmsenv;source /home/cmorgoth/Software/RootInstall/root/bin/thisroot.sh\ncp -r $motherDir .\ncd RazorFramework/HggRazor/FitMgg/\nmake clean;make;mkdir output\n./MakeFitRun2 --inputFile=DoublePhoton_22Jan2013_Run2012ABCD_v9.root --MRcut=$MR --RSQcut=$RSQ --treeName=SusyHggTree --dataMode=data --category=$category --fitMode=biasSignal --f1=$i --f2=$j --outputFile=$outName --invertedIso=$Iso\ncp $outName /mnt/hadoop/store/user/cmorgoth/$outDir/.\necho FINISHING!!!\n" > $submitFile
+	echo -e "#!/bin/sh\nexport SCRAM_ARCH=slc5_amd64_gcc472\nsource /cvmfs/cms.cern.ch/cmsset_default.sh\nexport LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/cmorgoth/git/BSMatLHC/hepmc/lib/\nmkdir $tmpDir\ncd $tmpDir\n\ncmsrel CMSSW_6_2_0;cd CMSSW_6_2_0/src;cmsenv;source /home/cmorgoth/Software/RootInstall/root/bin/thisroot.sh\ncp -r $motherDir .\ncd RazorFramework/HggRazor/FitMgg/\nmake clean;make;mkdir output\n./MakeFitRun2 --inputFile=DoublePhoton_22Jan2013_Run2012ABCD_v9.root --MRcut=$MR --RSQcut=$RSQ --treeName=SusyHggTree --dataMode=data --category=$category --fitMode=biasSignal --f1=$i --f2=$j --outputFile=$outName --invertedIso=$Iso --signalFraction=$sfrac\ncp $outName /mnt/hadoop/store/user/cmorgoth/$outDir/.\necho FINISHING!!!\n" > $submitFile
 	echo -e "Executable = $workingDir/$submitFile\nUniverse = vanilla\nOutput = $subOut\nLog = $subLog\nError = $subError\ngetenv = True\n\nQueue" > $condorFile
 	condor_submit $condorFile
 	sleep 0.1
--- HggRazor/FitMgg/src/PrintAIC.cc
+++ HggRazor/FitMgg/src/PrintAIC.cc
@@ -10,6 +10,7 @@
 #include <TPad.h>
 #include <TAxis.h>
 #include <TLegend.h>
+#include <TGraph.h>
 //ROOFIT INCLUDES
 #include <RooWorkspace.h>
 #include <RooPlot.h>
@@ -76,9 +77,12 @@ void PrintAICTable(std::string MRcut,std::string RSQcut,std::map<std::string, do
 	//double weight_aic_2[7]={weight_aic[0],weight_aic[5],weight_aic[1],weight_aic[6],weight_aic[3],weight_aic[4],weight_aic[2]};
 
 	gStyle->SetTitleSize(0.15,"t");
+	int n_g = 20;
+        TGraph *grshade[8];
+	
 	TCanvas *myC= new TCanvas("c1","c1",200,10,800,600);
 	myC->cd();
-    gPad->SetLeftMargin(0.12);
+	gPad->SetLeftMargin(0.12);
 	gPad->SetBottomMargin(0.18);
 
 	myC->Divide(2,4,0);
@@ -86,12 +90,25 @@ void PrintAICTable(std::string MRcut,std::string RSQcut,std::map<std::string, do
 	RooPlot * p1 = (RooPlot*)w[0]->obj("sideband_fit_doubleExp_pdf_dExp_N1N2_frame");
 	p1->SetTitle("double exponential");
 	p1->GetXaxis()->SetTitleSize(0.09);
-    p1->GetXaxis()->SetLabelSize(0.11);
+	p1->GetXaxis()->SetLabelSize(0.11);
 	p1->GetYaxis()->SetTitleSize(0.11);
 	p1->GetYaxis()->SetLabelSize(0.11);
 	p1->GetYaxis()->SetTitleOffset(0.55);
 	p1->GetYaxis()->SetNdivisions(505);
-p1->Draw();
+	p1->Draw();
+	
+	double top_y = p1->GetMaximum();
+	grshade[0] = new TGraph(2*(n_g+1));
+	for ( int ig = 0; ig <= n_g; ig++ )
+	  {
+	    grshade[0]->SetPoint(ig, 120., (float)ig*(top_y/(float)n_g) );
+	    grshade[0]->SetPoint( (n_g+1) + ig, 135., top_y - (float)ig*(top_y/(float)n_g) );
+	  }
+	//grshade->SetFillStyle(3013);
+	grshade[0]->SetFillStyle(1001);  
+	grshade[0]->SetFillColor(kRed-9);
+	grshade[0]->Draw("f same");
+	
 	TLegend* leg[8];
 	for(int idx=0;idx<8;idx++)
 	{
@@ -102,85 +119,169 @@ p1->Draw();
 	leg[idx]->SetBorderSize(0);
 	leg[idx]->SetTextColor(kRed);
 	}
-leg[0]->Draw();
-
+	leg[0]->Draw();
+		
 	myC->cd(2);
 	RooPlot * p2 = (RooPlot*)w[1]->obj("sideband_fit_singleExp_pdf_sExp_ext_frame");
 	p2->SetTitle("single exponential");
 	p2->GetXaxis()->SetTitleSize(0.09);
-    p2->GetXaxis()->SetLabelSize(0.11);
+	p2->GetXaxis()->SetLabelSize(0.11);
 	p2->GetYaxis()->SetTitleSize(0.11);
 	p2->GetYaxis()->SetLabelSize(0.11);
 	p2->GetYaxis()->SetTitleOffset(0.55);
 	p2->GetYaxis()->SetNdivisions(505);
-p2->Draw();
-leg[1]->Draw();
+	p2->Draw();
+	
+	top_y = p1->GetMaximum();
+	grshade[1] = new TGraph(2*(n_g+1));
+        for ( int ig = 0; ig <= n_g; ig++ )
+          {
+            grshade[1]->SetPoint(ig, 120., (float)ig*(top_y/(float)n_g) );
+            grshade[1]->SetPoint( (n_g+1) + ig, 135., top_y - (float)ig*(top_y/(float)n_g) );
+          }
+	grshade[1]->SetFillStyle(1001);
+        grshade[1]->SetFillColor(kRed-9);
+        grshade[1]->Draw("f same");
+	leg[1]->Draw();
 	myC->cd(3);
 	RooPlot * p3 = (RooPlot*)w[3]->obj("sideband_fit_doublePow_dpow_ext_frame");
 	p3->SetTitle("double power");
 	p3->GetXaxis()->SetTitleSize(0.09);
-    p3->GetXaxis()->SetLabelSize(0.11);
+	p3->GetXaxis()->SetLabelSize(0.11);
 	p3->GetYaxis()->SetTitleSize(0.11);
 	p3->GetYaxis()->SetLabelSize(0.11);
 	p3->GetYaxis()->SetTitleOffset(0.55);
 	p3->GetYaxis()->SetNdivisions(505);
-p3->Draw();
-leg[2]->Draw();
+	p3->Draw();
+	//making blind band 
+	top_y = p1->GetMaximum();
+        grshade[2] = new TGraph(2*(n_g+1));
+        for ( int ig = 0; ig <= n_g; ig++ )
+          {
+            grshade[2]->SetPoint(ig, 120., (float)ig*(top_y/(float)n_g) );
+            grshade[2]->SetPoint( (n_g+1) + ig, 135., top_y - (float)ig*(top_y/(float)n_g) );
+          }
+        grshade[2]->SetFillStyle(1001);
+        grshade[2]->SetFillColor(kRed-9);
+        grshade[2]->Draw("f same");
+	
+	leg[2]->Draw();
 	myC->cd(4);
 	RooPlot * p4 = (RooPlot*)w[2]->obj("sideband_fit_singlePow_spow_ext_frame");
 	p4->SetTitle("single power");
 	p4->GetXaxis()->SetTitleSize(0.09);
-    p4->GetXaxis()->SetLabelSize(0.11);
+	p4->GetXaxis()->SetLabelSize(0.11);
 	p4->GetYaxis()->SetTitleSize(0.11);
 	p4->GetYaxis()->SetLabelSize(0.11);
 	p4->GetYaxis()->SetTitleOffset(0.55);
 	p4->GetYaxis()->SetNdivisions(505);
-p4->Draw();
-leg[3]->Draw();
+	p4->Draw();
+	
+	top_y = p1->GetMaximum();
+        grshade[3] = new TGraph(2*(n_g+1));
+        for ( int ig = 0; ig <= n_g; ig++ )
+          {
+            grshade[3]->SetPoint(ig, 120., (float)ig*(top_y/(float)n_g) );
+            grshade[3]->SetPoint( (n_g+1) + ig, 135., top_y - (float)ig*(top_y/(float)n_g) );
+          }
+        grshade[3]->SetFillStyle(1001);
+        grshade[3]->SetFillColor(kRed-9);
+        grshade[3]->Draw("f same");
+	
+	leg[3]->Draw();
 	myC->cd(5);
 	RooPlot * p5 = (RooPlot*)w[4]->obj("sideband_fit_poly2_pol2_ext_frame");
 	p5->SetTitle("second order poly");
 	p5->GetXaxis()->SetTitleSize(0.09);
-    p5->GetXaxis()->SetLabelSize(0.11);
+	p5->GetXaxis()->SetLabelSize(0.11);
 	p5->GetYaxis()->SetTitleSize(0.11);
 	p5->GetYaxis()->SetLabelSize(0.11);
 	p5->GetYaxis()->SetTitleOffset(0.55);
 	p5->GetYaxis()->SetNdivisions(505);
-p5->Draw();
-leg[4]->Draw();
+	p5->Draw();
+	
+	top_y = p1->GetMaximum();
+        grshade[4] = new TGraph(2*(n_g+1));
+        for ( int ig = 0; ig <= n_g; ig++ )
+          {
+            grshade[4]->SetPoint(ig, 120., (float)ig*(top_y/(float)n_g) );
+            grshade[4]->SetPoint( (n_g+1) + ig, 135., top_y - (float)ig*(top_y/(float)n_g) );
+          }
+        grshade[4]->SetFillStyle(1001);
+        grshade[4]->SetFillColor(kRed-9);
+        grshade[4]->Draw("f same");
+	
+	leg[4]->Draw();
 	myC->cd(6);
 	RooPlot * p6 = (RooPlot*)w[5]->obj("sideband_fit_poly3_pol3_ext_frame");
 	p6->SetTitle("third order poly");
 	p6->GetXaxis()->SetTitleSize(0.09);
-    p6->GetXaxis()->SetLabelSize(0.11);
+	p6->GetXaxis()->SetLabelSize(0.11);
 	p6->GetYaxis()->SetTitleSize(0.11);
 	p6->GetYaxis()->SetLabelSize(0.11);
 	p6->GetYaxis()->SetTitleOffset(0.55);
 	p6->GetYaxis()->SetNdivisions(505);
-p6->Draw();
-leg[5]->Draw();
+	p6->Draw();
+
+	top_y = p1->GetMaximum();
+        grshade[5] = new TGraph(2*(n_g+1));
+        for ( int ig = 0; ig <= n_g; ig++ )
+          {
+            grshade[5]->SetPoint(ig, 120., (float)ig*(top_y/(float)n_g) );
+            grshade[5]->SetPoint( (n_g+1) + ig, 135., top_y - (float)ig*(top_y/(float)n_g) );
+          }
+        grshade[5]->SetFillStyle(1001);
+        grshade[5]->SetFillColor(kRed-9);
+        grshade[5]->Draw("f same");
+	
+	leg[5]->Draw();
 	myC->cd(7);
 	RooPlot * p7 = (RooPlot*)w[7]->obj("sideband_fit_poly4_pol4_ext_frame");
 	p7->SetTitle("fourth order poly");
 	p7->GetXaxis()->SetTitleSize(0.09);
-    p7->GetXaxis()->SetLabelSize(0.11);
+	p7->GetXaxis()->SetLabelSize(0.11);
 	p7->GetYaxis()->SetTitleSize(0.11);
 	p7->GetYaxis()->SetLabelSize(0.11);
 	p7->GetYaxis()->SetTitleOffset(0.55);
 	p7->GetYaxis()->SetNdivisions(505);
-p7->Draw();
-leg[6]->Draw();
+	p7->Draw();
+
+	top_y = p1->GetMaximum();
+        grshade[6] = new TGraph(2*(n_g+1));
+        for ( int ig = 0; ig <= n_g; ig++ )
+          {
+            grshade[6]->SetPoint(ig, 120., (float)ig*(top_y/(float)n_g) );
+            grshade[6]->SetPoint( (n_g+1) + ig, 135., top_y - (float)ig*(top_y/(float)n_g) );
+          }
+        grshade[6]->SetFillStyle(1001);
+        grshade[6]->SetFillColor(kRed-9);
+        grshade[6]->Draw("f same");
+	
+	leg[6]->Draw();
 	myC->cd(8);
 	RooPlot * p8 = (RooPlot*)w[6]->obj("sideband_fit_modExp_mexp_ext_frame");
 	p8->SetTitle("modified exponential");
 	p8->GetXaxis()->SetTitleSize(0.09);
-    p8->GetXaxis()->SetLabelSize(0.11);
+	p8->GetXaxis()->SetLabelSize(0.11);
 	p8->GetYaxis()->SetTitleSize(0.11);
 	p8->GetYaxis()->SetLabelSize(0.11);
 	p8->GetYaxis()->SetTitleOffset(0.55);
 	p8->GetYaxis()->SetNdivisions(505);
-p8->Draw();
-leg[7]->Draw();
+	p8->Draw();
+	
+	top_y = p1->GetMaximum();
+        grshade[7] = new TGraph(2*(n_g+1));
+        for ( int ig = 0; ig <= n_g; ig++ )
+          {
+            grshade[7]->SetPoint(ig, 120., (float)ig*(top_y/(float)n_g) );
+            grshade[7]->SetPoint( (n_g+1) + ig, 135., top_y - (float)ig*(top_y/(float)n_g) );
+          }
+        grshade[7]->SetFillStyle(1001);
+        grshade[7]->SetFillColor(kRed-9);
+        grshade[7]->Draw("f same");
+	
+	leg[7]->Draw();
+	
 /*	myC->cd(8);
 	TLegend* leg8 = new TLegend(0.2, 0.25, 0.8, 0.75);
 	leg8->AddEntry(p1,Form("M_{R} cut: %s", MRcut.c_str()),"");
