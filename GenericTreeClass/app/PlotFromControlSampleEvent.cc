41ebdaddacb6a010fd47c4a4a00e54c9147fcb11
cmorgoth <cristian.pena@caltech.edu>
2015-11-03 14:11:32 -0800

    add app

---
+++ GenericTreeClass/app/PlotFromControlSampleEvent.cc
@@ -0,0 +1,111 @@
+//C++ INCLUDES
+#include <iostream>
+#include <map>
+//ROOT INCLUDES
+#include <TString.h>
+#include <THStack.h>
+//LOCAL INCLUDES
+#include "ControlSampleEvent.hh"
+#include "GenericTreeClass.hh"
+#include "HggAux.hh"
+#include "CommandLineInput.hh"
+#include "TChainTools.hh"
+#include "PlotCosmetics.hh"
+
+//--------------------------
+//HighPt
+const int N_HighPt = 5;
+float MR_HighPt[N_HighPt+1] = {150,200,300,500,1600,3000};
+float Rsq_HighPt[N_HighPt+1] = {0.01,0.05,0.10,0.15,0.20,1.00};
+
+//HighRes
+const int N_HighRes = 4;
+float MR_HighRes[N_HighRes+1] = {150,250,400,1400,3000};
+float Rsq_HighRes[N_HighRes+1] = {0.01,0.05,0.10,0.15,1.00};
+
+//Hbb
+const int N_Hbb = 2;
+float MR_Hbb[N_Hbb+1] = {150,300,3000};
+float Rsq_Hbb[N_Hbb+1] = {0.01,0.05,1.00};
+
+float hpt_k[2]  = { 1.0, 1.0};
+float hres_k[2] = { 1.0, 1.0};
+float lres_k[2] = { 1.0, 1.0};
+//A p p l y   B a s e l i n e   C u t
+//-----------------------------------
+//DATA
+//EBEB
+TString cut = "pho1passEleVeto == 0 && pho2passEleVeto == 0 && pho1passIso == 1 && pho2passIso == 1 && abs( pho1Eta ) < 1.479 && abs( pho2Eta ) < 1.479 && ( HLTDecision[30] == 1 || HLTDecision[31] == 1 ) && mGammaGamma>60. && mGammaGamma<120. && pho1Pt>30 && pho2Pt>20";
+
+//MC
+//EBEB
+TString cut_mc = "pho1passEleVeto == 0 && pho2passEleVeto == 0 && pho1passIso == 1 && pho2passIso == 1 && abs( pho1Eta ) < 1.479 && abs( pho2Eta ) < 1.479  && ( HLTDecision[30] == 1 || HLTDecision[31] == 1 ) && mGammaGamma>60. && mGammaGamma<120. && pho1Pt>30 && pho2Pt>20";
+
+TString mggCut = "1";
+
+#define _debug 1
+
+int main ( int argc, char* argv[] )
+{
+  std::cout << "[INFO]: Initializing program" << std::endl;
+    
+  //Map Containing the lists for different processes 
+  std::map< std::string, std::string > mapList;
+  std::string inputFile = ParseCommandLine( argc, argv, "-inputFile=" );
+  if (  inputFile == "" )
+    {
+      std::cerr << "[ERROR]: please provide an input file using --inputFile=<path_to_file>" << std::endl;
+      return -1;
+    }
+  
+  std::string run = ParseCommandLine( argc, argv, "-run=" );
+  if (  run == "" )
+    {
+      std::cout << "[WARNING]: please provide a valid run, use --run=<run1/run2>" << std::endl;
+      run = "run1";
+    }
+
+  std::string treeType = ParseCommandLine( argc, argv, "-treeType=" );
+  if (  treeType == "" )
+    {
+      std::cout << "[WARNING]: please provide a valid treeType, use --treeType=<inclusive/category>" << std::endl;
+      treeType = "inclusive";
+    }
+  
+  std::cout << "=================================" << std::endl;
+  std::cout << "===========set parameters========" << std::endl;
+  std::cout << "[INFO]: input file: " << inputFile << std::endl;
+  std::cout << "[INFO]: run: " << run << std::endl;
+  std::cout << "=================================" << std::endl;
+  
+  FillMapList( mapList, inputFile );
+  if ( _debug ) std::cout << "[DEBUG]: map size: " << mapList.size() << std::endl;
+  
+  for( auto& myMap : mapList )
+    {
+      if ( _debug ) std::cout << "[DEBUG]: first: " << myMap.first << " second: " << myMap.second << std::endl;
+    }
+  
+  TFile* f;
+  TTree* tree;
+  TChain* chain;
+  TTree* cutTree;
+  GenericTreeClass* myClass = new GenericTreeClass();
+  THStack* stack;
+  TLegend* leg;
+  TH1F* data;
+  TH1F* mc;
+  TH1F* mc2 = new TH1F();
+
+  const int nprocesses = 2;
+  const int nplots = 4;
+  double k_f = 1.0;
+  const double lumi_frac = 1.0; // (5./19.8)
+  const int mod = 0; 
+
+  myClass->CreateGenericHisto( "mr", "MR", 250, 0, 2500 );
+  myClass->PrintStoredHistos();
+  
+  myClass->Loop();
+  return 0;
+}
