777bcd68b4aca64c37697f4087395532769f8f5b
Zhicai Zhang <zzhang2@caltech.edu>
2016-02-28 17:24:15 -0800

    adding crystall ball func

--- HggRazor/FitMgg/src/PlotBias.cc
+++ HggRazor/FitMgg/src/PlotBias.cc
@@ -431,7 +431,27 @@ if (fitFunc == "singleGaus")
   return mu_value;
 };
 
-
+double crystalball_function(double x, double alpha, double n, double sigma, double mean)
+{
+          // evaluate the crystal ball function
+          if (sigma < 0.)     return 0.;
+          double z = (x - mean)/sigma; 
+          if (alpha < 0) z = -z; 
+          double abs_alpha = std::abs(alpha);
+         // double C = n/abs_alpha * 1./(n-1.) * std::exp(-alpha*alpha/2.);
+         // double D = std::sqrt(M_PI/2.)*(1.+ROOT::Math::erf(abs_alpha/std::sqrt(2.)));
+         // double N = 1./(sigma*(C+D));
+         if (z  > - abs_alpha)
+            return std::exp(- 0.5 * z * z);
+         else {
+            //double A = std::pow(n/abs_alpha,n) * std::exp(-0.5*abs_alpha*abs_alpha);
+            double nDivAlpha = n/abs_alpha;
+            double AA =  std::exp(-0.5*abs_alpha*abs_alpha);
+            double B = nDivAlpha -abs_alpha;
+            double arg = nDivAlpha/(B-z);
+            return AA * std::pow(arg,n);
+         }
+      }
 
 
 
