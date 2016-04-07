001285af6d428543064774f8c4aef7bbb16223c1
cmorgoth <cristian.morgoth@gmail.com>
2016-04-06 17:22:34 -0700

    updating

---
+++ HggRazor/LimitTools/app/testMinNll.cc
@@ -0,0 +1,46 @@
+#include <assert.h>
+//ROOT INCLUDES
+#include <TFile.h>
+#include <TTree.h>
+#include <TH1F.h>
+#include <TROOT.h>
+#include <TRandom3.h>
+//LOCAL INCLUDES
+#include "AssortedFunctions.hh"
+#include "CommandLineInput.hh"
+
+int main( int argc, char* argv[] )
+{
+  gROOT->Reset();
+  const int n = 2;
+  double b_nr[n]  = {10, 5};//non-resonant bkg
+  double b_smh[n] = {1, 1};//smh bkg
+  
+  
+  double b[n];
+  double s[n] = {10, 20};//signal is fixed
+  double obs[n];
+
+  TRandom3 r1(0);
+  TRandom3 r2(0);
+  TH1F* h_qnot = new TH1F("h_qnot", "h_qnot", 1000, -1000, 1000);
+  for ( int i = 0; i < 100; i ++ )
+    {
+      for ( int j = 0; j < n; j++ )
+	{
+	  b[j] = b_nr[j] + b_nr[j];
+	  double bNonRes = r1.PoissonD( b_nr[j] );
+	  double bSMH = r2.PoissonD( b_smh[j] );
+	  obs[j] = bNonRes + bSMH;
+	}
+      double mu   = GetBestFitSignalStrength( n, b, s, obs );
+      double qnot = GetQnotTestStatistics( n, b, s, obs, mu );
+      h_qnot->Fill( qnot );
+    }
+  
+  TFile* fout = new TFile("test_qnot.root", "RECREATE");
+  h_qnot->Write();
+  fout->Close();
+  
+  return 0;
+}
--- HggRazor/LimitTools/include/AssortedFunctions.hh
+++ HggRazor/LimitTools/include/AssortedFunctions.hh
@@ -2,3 +2,5 @@
 
 
 double GetEffSigma( TH1* hist );
+double GetBestFitSignalStrength( int n, double* b, double* s, double* obs );
+double GetQnotTestStatistics( int n, double* b, double* s, double* obs, double mu );
