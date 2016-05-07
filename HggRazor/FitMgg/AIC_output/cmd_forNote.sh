489e28db74819a01b94d2f2cc1494733b68e8b56
Zhicai <zzhang2@caltech.edu>
2016-05-07 05:55:50 +0200

    updating..

--- HggRazor/FitMgg/AIC_output/cmd_forNote.sh
+++ HggRazor/FitMgg/AIC_output/cmd_forNote.sh
@@ -2,6 +2,7 @@ rm aic_tables.tex aic_plots.tex
 cat FitChoices_Table_highpt_ColorTable_WithStatus.tex FitChoices_Table_highres_ColorTable_WithStatus.tex FitChoices_Table_lowres_ColorTable_WithStatus.tex FitChoices_Table_hzbb_ColorTable_WithStatus.tex >> aic_tables.tex
 cat FitChoices_Plot_highpt.tex FitChoices_Plot_highres.tex FitChoices_Plot_lowres.tex FitChoices_Plot_hzbb.tex >> aic_plots.tex
 sed -i "s/_0\./_0d/g" `grep _0\. -rl aic_plots.tex`
+sed -i "s/hbtp/h/g" `grep _0\. -rl aic_plots.tex`
 python renamePDF.py highpt
 python renamePDF.py highres
 python renamePDF.py lowres
@@ -13,3 +14,4 @@ cp -r lowres /afs/cern.ch/user/z/zhicaiz/private/CMS_AN/tdr2/notes/AN-16-129/tru
 cp -r hzbb /afs/cern.ch/user/z/zhicaiz/private/CMS_AN/tdr2/notes/AN-16-129/trunk/figs/
 rm /afs/cern.ch/user/z/zhicaiz/private/CMS_AN/tdr2/notes/AN-16-129/trunk/figs/*/*.root
 cp aic_tables.tex /afs/cern.ch/user/z/zhicaiz/private/CMS_AN/tdr2/notes/AN-16-129/trunk/
+cp aic_plots.tex /afs/cern.ch/user/z/zhicaiz/private/CMS_AN/tdr2/notes/AN-16-129/trunk/
