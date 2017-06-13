d565dc87a7202c89a020b765b387ae55725383e1
Zhicai <zzhang2@caltech.edu>
2017-06-13 02:06:18 +0200

    update bias tree fit

--- HggRazor/FitMgg/scripts/gen_bias_cmd.py
+++ HggRazor/FitMgg/scripts/gen_bias_cmd.py
@@ -7,10 +7,10 @@ import subprocess, time, sys, os, shlex
 
 print "RUNNING BIAS TEST"
 
-inputList = "/afs/cern.ch/work/z/zhicaiz/public/BiasSignalTest_result_test_25Apr2016_cmscaf1nd/trees/"
+inputList = "/afs/cern.ch/work/z/zhicaiz/public/BiasSignalTest_Leptons_35p9ifb_05June2017_cmscaf1nd_hacked/trees/"
 
-outputDir ="Bias_output/Bias_plots/"
-SoverB = ["0.0","1.0", "3.0", "5.0"]
+outputDir ="Bias_output/Bias_plots"
+SoverB = ["0.0"]
 
 AICw = ["AICw1", "AICw2","AICw3","AICw4","AICw5","AICw6","AICw7"]
 AICfunc = ["AICfunc1", "AICfunc2","AICfunc3","AICfunc4","AICfunc5","AICfunc6","AICfunc7"]
@@ -23,15 +23,15 @@ if __name__ == "__main__":
 	pwd = os.getcwd()
 	work_directory = pwd.replace("scripts","")
 	
-	env_script_no = open("bias_plot_status_no_cmd.sh","w")
+	#env_script_no = open("bias_plot_status_no_cmd.sh","w")
 	env_script_yes = open("bias_plot_status_yes_cmd.sh","w")
 
 	with open(bin_list_filename,"r") as bin_list_file:
 		for this_bin in bin_list_file:
 			bin_array = shlex.split(this_bin)
 			for SoverB_index in range(len(SoverB)):
-				env_script_no.write("mkdir -p "+outputDir+"/"+bin_array[0]+"_"+bin_array[1]+"_"+bin_array[3]+"/SoB_"+SoverB[SoverB_index] + "\n")
-				env_script_no.write("./PlotAllBiasTest --fitFunc=doublecrystalBall --inputFile="+inputList+bin_array[0]+"_"+bin_array[1]+"_"+bin_array[3]+"/SoB_"+SoverB[SoverB_index]+"/Tree/list.list --mode=table2 --requireFitStatus=no --category="+bin_array[0]+" --SoB="+SoverB[SoverB_index]+" --LowMRcut="+bin_array[1]+" --HighMRcut="+bin_array[2]+" --LowRSQcut="+bin_array[3]+" --HighRSQcut="+bin_array[4]+" ")	
+				env_script_yes.write("mkdir -p "+outputDir+"/"+bin_array[0]+"_"+bin_array[1]+"_"+bin_array[3]+"/SoB_"+SoverB[SoverB_index] + "\n")
+				env_script_yes.write("./PlotAllBiasTest --fitFunc=doublecrystalBall --inputFile="+inputList+bin_array[0]+"_"+bin_array[1]+"_"+bin_array[3]+"/SoB_"+SoverB[SoverB_index]+"/Tree/list.list --mode=table2 --requireFitStatus=yes --category="+bin_array[0]+" --SoB="+SoverB[SoverB_index]+" --LowMRcut="+bin_array[1]+" --HighMRcut="+bin_array[2]+" --LowRSQcut="+bin_array[3]+" --HighRSQcut="+bin_array[4]+" ")	
 				for fidx in range((len(bin_array)-5)/2):
-					env_script_no.write(" --"+AICfunc[fidx]+"="+bin_array[fidx*2+5]+" --"+AICw[fidx]+"="+bin_array[fidx*2+6])
-				env_script_no.write(" --outputDir="+outputDir+ "/"+bin_array[0]+"_"+bin_array[1]+"_"+bin_array[3]+"/SoB_"+SoverB[SoverB_index]+ " > "+outputDir+ "/"+bin_array[0]+"_"+bin_array[1]+"_"+bin_array[3]+"/SoB_"+SoverB[SoverB_index]+"/log.log \n")
+					env_script_yes.write(" --"+AICfunc[fidx]+"="+bin_array[fidx*2+5]+" --"+AICw[fidx]+"="+bin_array[fidx*2+6])
+				env_script_yes.write(" --outputDir="+outputDir+ "/"+bin_array[0]+"_"+bin_array[1]+"_"+bin_array[3]+"/SoB_"+SoverB[SoverB_index]+ " > "+outputDir+ "/"+bin_array[0]+"_"+bin_array[1]+"_"+bin_array[3]+"/SoB_"+SoverB[SoverB_index]+"/log.log \n")
